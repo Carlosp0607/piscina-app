7b10c628dbac0b9badcf48c8fc74635dd5e85e8b
Carlos Daniel Polanco <carlosdanielpolanco0@gmail.com>
2026-09-17 23:42:52 -0500

    Add authorization middleware to miembro routes

--- src/routes/miembros.js
+++ src/routes/miembros.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const miembroController = require('../controllers/miembroController');
+const { permitir } = require('../middleware/auth');
 
-router.get('/', miembroController.getAll);
-router.get('/:id', miembroController.getById);
-router.post('/', miembroController.create);
-router.put('/:id', miembroController.update);
-router.delete('/:id', miembroController.delete);
+const lectura = permitir('admin', 'portero', 'guest', 'guest-portero');
+const escritura = permitir('admin');
+
+router.get('/', lectura, miembroController.getAll);
+router.get('/:id', lectura, miembroController.getById);
+router.post('/', escritura, miembroController.create);
+router.put('/:id', escritura, miembroController.update);
+router.delete('/:id', escritura, miembroController.delete);
 
 module.exports = router;
